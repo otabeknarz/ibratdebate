0a5beb306b1ca23154d56bbcd61dd9a3c3e30fd7
Otabek Narziqulov <otabeknarz@gmail.com>
2024-12-12 17:38:35 +0500

    i dont know what is that and how it was created 🤣

--- core/translation.py
+++ core/translation.py
@@ -1,7 +1,9 @@
 from modeltranslation.translator import translator, TranslationOptions
 from .models import Debate
 
+
 class DebateTranslationOptions(TranslationOptions):
-    fields = ('name', 'description')
+    fields = ("name", "description")
+
 
 translator.register(Debate, DebateTranslationOptions)
