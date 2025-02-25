911114834777096423216d45d45bbff13bb1e892
Otabek Narz <otabeknarz@gmail.com>
2025-02-25 14:07:16 +0500

    fixed static_root for production

--- ibratdebate/settings.py
+++ ibratdebate/settings.py
@@ -138,8 +138,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-# STATIC_ROOT = "static/"
-STATICFILES_DIRS = (BASE_DIR / "static/",)
+STATIC_ROOT = "static/"
+# STATICFILES_DIRS = (BASE_DIR / "static/",)
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media/"
