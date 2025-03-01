188179eaa616a7c13ab94ac32c9c2368c0a68405
Otabek Narz <otabeknarz@gmail.com>
2025-03-01 21:12:29 +0500

    restored qr_code_scanner() and auth urls

--- core/views.py
+++ core/views.py
@@ -55,69 +55,73 @@ from .models import Debate, Ticket, User
 #     return render(request, "team.html", {"team_members": team_members})
 #
 #
-# def login_view(request):
-#     if request.method == "POST":
-#         username = request.POST["username"]
-#         password = request.POST["password"]
-#
-#         user = get_user_model().objects.filter(username=username).first()
-#
-#         if not user:
-#             return redirect("core:login")
-#
-#         if user.check_password(password):
-#             login(request, user=user)
-#             return redirect("core:home")
-#         else:
-#             return redirect("core:login")
-#
-#     return render(request, "auth/login.html")
-#
-#
-# @login_required(login_url="core:login")
-# def logout_view(request):
-#     logout(request)
-#     return redirect("core:login")
-#
-#
-# def stats(request):
-#     debates = Debate.objects.filter(is_expired=False)
-#     data = []
-#     for debate in debates:
-#         data.append(
-#             [
-#                 debate,
-#                 debate.people.all().count(),
-#                 localtime(debate.date).strftime("%d/%m/%Y | %H:%M"),
-#             ]
-#         )
-#     return render(request, "stats.html", {"data": data})
-#
-#
-# def qr_code_scanner_view(request):
-#     if request.method == "POST" and request.user.is_authenticated:
-#         if request.user.role not in (1, 2):
-#             return redirect("core:home")
-#         qr_code_data = request.POST.get("qr_code_data")
-#
-#         try:
-#             uuid.UUID(qr_code_data, version=4)
-#         except ValueError:
-#             return render(request, "qr_code_scanner.html", {"error": True, "error_message": "Invalid QR code"})
-#
-#         ticket = Ticket.objects.filter(id=qr_code_data).first()
-#         if not ticket:
-#             return render(request, "qr_code_scanner.html", {"error": True, "error_message": "Ticket not found"})
-#         if ticket.is_used:
-#             return render(request, "qr_code_scanner.html", {"error": True, "error_message": "Ticket already used"})
-#         ticket.is_used = True
-#         ticket.save()
-#         Ticket.objects.filter(user=ticket.user, is_used=False).delete()
-#         return render(request, "qr_code_scanner.html", {"success": True, "ticket": ticket, "debater": ticket.user})
-#
-#     if not request.user.is_authenticated:
-#         return render(request, "auth/login.html")
-#     elif request.user.role in (1, 2):
-#         return render(request, "qr_code_scanner.html")
-#     else:
-#         return redirect("core:home")
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = get_user_model().objects.filter(username=username).first()
+
+        if not user:
+            return redirect("core:login")
+
+        if user.check_password(password):
+            login(request, user=user)
+            return redirect("core:home")
+        else:
+            return redirect("core:login")
+
+    return render(request, "auth/login.html")
+
+
+@login_required(login_url="core:login")
+def logout_view(request):
+    logout(request)
+    return redirect("core:login")
+
+
+def stats(request):
+    debates = Debate.objects.filter(is_expired=False)
+    data = []
+    for debate in debates:
+        data.append(
+            [
+                debate,
+                debate.people.all().count(),
+                localtime(debate.date).strftime("%d/%m/%Y | %H:%M"),
+            ]
+        )
+    return render(request, "stats.html", {"data": data})
+
+
+def qr_code_scanner_view(request):
+    if request.method == "POST" and request.user.is_authenticated:
+        if request.user.role not in (1, 3):
+            return redirect("core:home")
+        qr_code_data = request.POST.get("qr_code_data")
+
+        try:
+            uuid.UUID(qr_code_data, version=4)
+        except ValueError:
+            return render(request, "qr_code_scanner.html", {"error": True, "error_message": "Invalid QR code"})
+
+        ticket = Ticket.objects.filter(id=qr_code_data).first()
+        if not ticket:
+            return render(request, "qr_code_scanner.html", {"error": True, "error_message": "Ticket not found"})
+        if ticket.is_used:
+            return render(request, "qr_code_scanner.html", {"error": True, "error_message": "Ticket already used"})
+        ticket.is_used = True
+        ticket.save()
+        Ticket.objects.filter(user=ticket.user, is_used=False).delete()
+        return render(request, "qr_code_scanner.html", {"success": True, "ticket": ticket, "debater": ticket.user})
+
+    if not request.user.is_authenticated:
+        return render(request, "auth/login.html")
+    elif request.user.role in (1, 2):
+        return render(request, "qr_code_scanner.html")
+    else:
+        return redirect("core:home")
+
+
+def error_404(request):
+    return HttpResponse("<h1>404</h1>", content_type="text/html")
