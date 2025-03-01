6cc09839c869c9ddba2c6f13f9147090993c66ff
Otabek Narz <otabeknarz@gmail.com>
2025-03-01 21:12:15 +0500

    restored qr_code_scanner() and auth urls

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from . import views
 
-# app_name = "core"
+app_name = "core"
 #
-# urlpatterns = [
-#     path("", views.home, name="home"),
+urlpatterns = [
+    path("404", views.error_404, name="home"),
 #     path("purpose/", views.purpose, name="purpose"),
-#     path("login/", views.login_view, name="login"),
-#     path("logout/", views.logout_view, name="logout"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
 #     path(
 #         "previous_debate/<int:debate_id>/",
 #         views.previous_debate,
@@ -15,6 +15,6 @@ from . import views
 #     ),
 #     path("team/", views.team_view, name="team"),
 #     path("previous_debates/", views.previous_debates_view, name="previous_debates"),
-#     path("stats/", views.stats, name="stats"),
-#     path("qr_code_scanner/", views.qr_code_scanner_view, name="qr_code_scanner"),
-# ]
+    path("stats/", views.stats, name="stats"),
+     path("", views.qr_code_scanner_view, name="qr_code_scanner"),
+]
